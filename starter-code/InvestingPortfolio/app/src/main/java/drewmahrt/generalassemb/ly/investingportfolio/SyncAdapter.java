487dce6bf8c9e82b228526d6dc7fbf54ff3f84b2
Jason Chan <jchan122490@gmail.com>
2016-08-23 21:59:40 -0400

    was working with anders yesteray and forgot to commit i didnt get to finish only did up to step four got lost on step five

---
+++ starter-code/InvestingPortfolio/app/src/main/java/drewmahrt/generalassemb/ly/investingportfolio/SyncAdapter.java
@@ -0,0 +1,31 @@
+package drewmahrt.generalassemb.ly.investingportfolio;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by Qube on 8/22/16.
+ */
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+    ContentResolver mContentResolver;
+
+    public SyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        mContentResolver = context.getContentResolver();
+    }
+
+    public SyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        mContentResolver = context.getContentResolver();
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle bundle, String s, ContentProviderClient contentProviderClient, SyncResult syncResult) {
+
+    }
+}
